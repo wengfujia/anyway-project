1863c214232efa93bcc55ea184af6f6eb4bd9c38
wengfujia <wengfujia@sina.com>
2017-03-28 23:24:29 +0800

    代码调整

---
+++ server-parent/server-web/src/main/java/org/anyway/server/web/cache/ehcache/event/HttpCacheEventFactory.java
@@ -0,0 +1,23 @@
+package org.anyway.server.web.cache.ehcache.event;
+
+/*
+ * 名称: HttpCacheEventFactory
+ * 描述: HttpCacheEvent监听事件工厂类，则该工厂类协调处理监听
+ * 版本：  1.0.0
+ * 作者： 翁富家
+ * 修改:
+ * 日期：2015年6月4日
+ * 修改日期:
+ */
+import java.util.Properties;
+
+import net.sf.ehcache.event.CacheEventListener;
+import net.sf.ehcache.event.CacheEventListenerFactory;
+
+public class HttpCacheEventFactory extends CacheEventListenerFactory {
+
+	@Override
+    public CacheEventListener createCacheEventListener(Properties properties) {
+        return new HttpCacheEvent();
+    }
+}
